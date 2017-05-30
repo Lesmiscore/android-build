2470b401208eefd2436ed474649d118d9fc893b3
nao20010128nao <nao20010128@gmail.com>
2017-05-30 14:29:47 +0900

    修正

--- install_sdk_packages.sh
+++ install_sdk_packages.sh
@@ -1,52 +1,53 @@
 #!/bin/sh
-install_sdk_package "platform-tools" | grep 'package installed'
+install_sdk_package "platform-tools"
 
-install_sdk_package "platforms;android-25" | grep 'package installed'
-install_sdk_package "platforms;android-24" | grep 'package installed'
-install_sdk_package "platforms;android-23" | grep 'package installed'
-install_sdk_package "platforms;android-22" | grep 'package installed'
-install_sdk_package "platforms;android-21" | grep 'package installed'
-install_sdk_package "platforms;android-20" | grep 'package installed'
-install_sdk_package "platforms;android-19" | grep 'package installed'
-install_sdk_package "platforms;android-17" | grep 'package installed'
-install_sdk_package "platforms;android-15" | grep 'package installed'
-install_sdk_package "platforms;android-10" | grep 'package installed'
+install_sdk_package "platforms;android-25"
+install_sdk_package "platforms;android-24"
+install_sdk_package "platforms;android-23"
+install_sdk_package "platforms;android-22"
+install_sdk_package "platforms;android-21"
+install_sdk_package "platforms;android-20"
+install_sdk_package "platforms;android-19"
+install_sdk_package "platforms;android-17"
+install_sdk_package "platforms;android-15"
+install_sdk_package "platforms;android-10"
 
 
-install_sdk_package "build-tools;25.0.2" | grep 'package installed'
-install_sdk_package "build-tools;25.0.1" | grep 'package installed'
-install_sdk_package "build-tools;25.0.0" | grep 'package installed'
-install_sdk_package "build-tools;24.0.3" | grep 'package installed'
-install_sdk_package "build-tools;24.0.2" | grep 'package installed'
-install_sdk_package "build-tools;24.0.1" | grep 'package installed'
-install_sdk_package "build-tools;24.0.0" | grep 'package installed'
-install_sdk_package "build-tools;23.0.3" | grep 'package installed'
-install_sdk_package "build-tools;23.0.2" | grep 'package installed'
-install_sdk_package "build-tools;23.0.1" | grep 'package installed'
-install_sdk_package "build-tools;23.0.0" | grep 'package installed'
-install_sdk_package "build-tools;22.0.1" | grep 'package installed'
-install_sdk_package "build-tools;22.0.0" | grep 'package installed'
-install_sdk_package "build-tools;21.1.2" | grep 'package installed'
-install_sdk_package "build-tools;21.1.1" | grep 'package installed'
-install_sdk_package "build-tools;21.1.0" | grep 'package installed'
-install_sdk_package "build-tools;21.0.2" | grep 'package installed'
-install_sdk_package "build-tools;21.0.1" | grep 'package installed'
-install_sdk_package "build-tools;21.0.0" | grep 'package installed'
-install_sdk_package "build-tools;20.0.0" | grep 'package installed'
-install_sdk_package "build-tools;19.1.0" | grep 'package installed'
-install_sdk_package "build-tools;19.0.3" | grep 'package installed'
-install_sdk_package "build-tools;19.0.2" | grep 'package installed'
-install_sdk_package "build-tools;19.0.1" | grep 'package installed'
-install_sdk_package "build-tools;19.0.0" | grep 'package installed'
-install_sdk_package "build-tools;18.1.1" | grep 'package installed'
-install_sdk_package "build-tools;18.1.0" | grep 'package installed'
-install_sdk_package "build-tools;18.0.1" | grep 'package installed'
-install_sdk_package "build-tools;17.0.0" | grep 'package installed'
+install_sdk_package "build-tools;25.0.3"
+install_sdk_package "build-tools;25.0.2"
+install_sdk_package "build-tools;25.0.1"
+install_sdk_package "build-tools;25.0.0"
+install_sdk_package "build-tools;24.0.3"
+install_sdk_package "build-tools;24.0.2"
+install_sdk_package "build-tools;24.0.1"
+install_sdk_package "build-tools;24.0.0"
+install_sdk_package "build-tools;23.0.3"
+install_sdk_package "build-tools;23.0.2"
+install_sdk_package "build-tools;23.0.1"
+install_sdk_package "build-tools;23.0.0"
+install_sdk_package "build-tools;22.0.1"
+install_sdk_package "build-tools;22.0.0"
+install_sdk_package "build-tools;21.1.2"
+install_sdk_package "build-tools;21.1.1"
+install_sdk_package "build-tools;21.1.0"
+install_sdk_package "build-tools;21.0.2"
+install_sdk_package "build-tools;21.0.1"
+install_sdk_package "build-tools;21.0.0"
+install_sdk_package "build-tools;20.0.0"
+install_sdk_package "build-tools;19.1.0"
+install_sdk_package "build-tools;19.0.3"
+install_sdk_package "build-tools;19.0.2"
+install_sdk_package "build-tools;19.0.1"
+install_sdk_package "build-tools;19.0.0"
+install_sdk_package "build-tools;18.1.1"
+install_sdk_package "build-tools;18.1.0"
+install_sdk_package "build-tools;18.0.1"
+install_sdk_package "build-tools;17.0.0"
 
-install_sdk_package "extras;android;m2repository" | grep 'package installed'
-install_sdk_package "extras;google;m2repository" | grep 'package installed'
-install_sdk_package "extras;google;google_play_services" | grep 'package installed'
+install_sdk_package "extras;android;m2repository"
+install_sdk_package "extras;google;m2repository"
+install_sdk_package "extras;google;google_play_services"
 
-install_sdk_package "addon-google_apis-google-23" | grep 'package installed' || true
-install_sdk_package "addon-google_apis-google-22" | grep 'package installed' || true
-install_sdk_package "addon-google_apis-google-21" | grep 'package installed' || true
+install_sdk_package "addon-google_apis-google-23" || true
+install_sdk_package "addon-google_apis-google-22" || true
+install_sdk_package "addon-google_apis-google-21" || true
